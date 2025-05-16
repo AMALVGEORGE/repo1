d84ce43739668afa30feda1bc3086c9f0534a80e
amal4 <amal4@AmalVGeorge>
2025-05-16 20:43:33 +0530

    Locators

---
+++ Automation_Course/src/test/java/seleniumBasics/HandlngLocators.java
@@ -0,0 +1,28 @@
+package seleniumBasics;
+
+import org.openqa.selenium.By;
+
+public class HandlngLocators extends Base {
+	
+	public void verifyLocators()
+	{
+		
+		driver.findElement(By.id("button-one"));
+		driver.findElement(By.className("form-group"));
+		driver.findElement(By.tagName("button"));
+		driver.findElement(By.name("viewport"));
+		driver.findElement(By.linkText("Simple Form Demo"));
+		driver.findElement(By.partialLinkText("Simple Form"));
+		driver.findElement(By.cssSelector("input[id=['single-input-field']"));
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+      HandlngLocators locators = new HandlngLocators();
+      locators.intialisebrowser();
+      locators.verifyLocators();
+    // locators.browserclose();
+}
+}
